1455098f1c6b8fd14650d4629e751a9eb330aaa5
j.saidane <j.saidane@DESK-HP28>
2019-03-06 16:17:35 +0100

    Service Methode + interface for injection de dependance

--- Services/ClientsServices.cs
+++ Services/ClientsServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,23 @@ namespace Services
 {
     public class ClientsServices
     {
-
+        public ClientRepository _clientRepository => new ClientRepository();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Clients> GetAllClients()
+        {
+            return _clientRepository.GetClients();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public Clients GetClientById(string Id)
+        {
+            return _clientRepository.GetClientsById(Id);
+        }
     }
 }
